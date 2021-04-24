33ed933c8fb39c62fc0eccde6fa1913ba70f450e
Christopher Crook <christopher.crook@vanderbilt.edu>
2021-04-23 20:01:07 -0500

    Modified install file to add default jre

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 echo "Fetching Apache Pulsar"
+sudo apt install default-jre
 wget https://archive.apache.org/dist/pulsar/pulsar-2.7.1/apache-pulsar-2.7.1-bin.tar.gz
 tar xvfz apache-pulsar-2.7.1-bin.tar.gz
 pip3 install pulsar-client
