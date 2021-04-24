81d7cd16725b1f8fd844a082fa1f3fa5c460a524
Christopher Crook <christopher.crook@vanderbilt.edu>
2021-04-23 19:46:08 -0500

    Modified the install script

--- install.sh
+++ install.sh
@@ -1,8 +1,4 @@
 echo "Fetching Apache Pulsar"
 wget https://archive.apache.org/dist/pulsar/pulsar-2.7.1/apache-pulsar-2.7.1-bin.tar.gz
-cp apache-pulsar-2.7.1-bin.tar.gz ~
-cd ~
 tar xvfz apache-pulsar-2.7.1-bin.tar.gz
-cp run_consumer.sh ~/apache-pulsar-2.7.1
-cp run_producer.sh ~/apache-pulsar-2.7.1
 pip3 install pulsar-client
